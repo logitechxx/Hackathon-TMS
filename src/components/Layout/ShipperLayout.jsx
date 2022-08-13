d418037661428a49e884d51eef3ecf809a23080a
giwangdk <gidwikintan@gmail.com>
2022-08-13 14:14:45 +0700

    fix:fix adding outlet to layout

--- src/components/Layout/ShipperLayout.jsx
+++ src/components/Layout/ShipperLayout.jsx
@@ -1,11 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import NavbarShipper from './Navbar/NavbarShipper';
 
 function ShipperLayout() {
     return (
         <Box backgroundColor={"#282c34"}>
             <NavbarShipper />
+            <Outlet/>
         </Box>
     );
 }
