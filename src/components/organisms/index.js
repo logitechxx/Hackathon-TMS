71be050915a3a0ec1b5ac922bd34da30de05e1b6
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2022-08-13 16:46:04 +0700

    fix: style and remove some shipper functions

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -2,6 +2,6 @@ import TableTruck from "./Truck/TableTruck";
 import CreateTruck from "./Truck/Create";
 import EditTruck from "./Truck/Edit";
 import TableShipper from "./Shipper/TableShipper";
-import CreateShipper from "./Shipper/CreateShipper";
+import CreateShipper from "./Shipper/Create/CreateShipper";
 
 export {TableTruck, CreateTruck, TableShipper, CreateShipper, EditTruck}
--- src/pages/shipper/index.jsx
+++ src/pages/shipper/index.jsx
@@ -1,4 +1,4 @@
-import {Box, Button, Link, Text, Flex} from '@chakra-ui/react';
+import {Box, Button, Link, Text, Flex, Heading} from '@chakra-ui/react';
 import React from 'react';
 import { TableShipper } from '../../components'
 import CreateShipper from '../../components/organisms/Shipper/Create/CreateShipper'
@@ -10,7 +10,8 @@ function Shipper() {
             m="10"
             backgroundColor='linkedin.100'
             height='80vh'>
-            <Flex>
+            <Flex justifyContent={"space-between"}>
+                <Heading as={"h3"}>Shippment</Heading>
                 <CreateShipper />
             </Flex>
             <TableShipper />
