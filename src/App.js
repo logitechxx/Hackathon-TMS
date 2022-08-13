55012e2054a921231975624e2adf3f681458cadf
giwangdk <gidwikintan@gmail.com>
2022-08-13 11:44:54 +0700

    fix(layout): fix navbar layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout/TransportLayout';
-import { routes } from './route';
+import { routes, transporters } from './route';
 
 function App() {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route element={<Layout />} path="/">
           {routes.map((route) => {
+            const { element, path } = route;
+            return <Route key={path} path={path} element={element} />;
+          })}
+          <Route element={<Layout />} path="/">
+          {transporters.map((route) => {
             const { element, path } = route;
             return <Route key={path} path={path} element={element} />;
           })}
--- src/route/index.js
+++ src/route/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-const Home = React.lazy(() => import('../pages/Home'));
 const Transporters = React.lazy(() => import('../pages/Transporters'));
 const Login = React.lazy(() => import('../pages/login/'));
 
