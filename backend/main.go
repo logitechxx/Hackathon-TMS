24113d21a120ce0c21117fe52f0b1f193f24e2af
SamuelGirsang <samueelgrs@gmail.com>
2022-08-13 12:11:27 +0700

    (feat/truck): Implement add truck feature

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"kargo/TMS/core/services"
+	"kargo/TMS/handler"
+	"kargo/TMS/repositories"
 	"log"
 	"os"
 
@@ -11,18 +14,27 @@ import (
 func goDotEnvVariable(key string) string {
 
 	err := godotenv.Load(".env")
-  
+
 	if err != nil {
-	  log.Fatalf("Error loading .env file")
+		log.Fatalf("Error loading .env file")
 	}
-  
+
 	return os.Getenv(key)
 }
 
 func main() {
-	//dbURL := goDotEnvVariable("POSTGRES_URL")
+	dbURL := goDotEnvVariable("POSTGRES_URL")
+	DB := repositories.Init(dbURL)
+
+	truckRepository := repositories.NewTruckRepository(DB)
+	truckService := services.NewTruckService(truckRepository)
+	truckHandler := handler.NewTruckHandler(truckService)
 
 	router := gin.Default()
+
+	trucksRouter := router.Group("trucks")
+
+	trucksRouter.POST("/", truckHandler.Create)
 
 	router.Run()
 }
