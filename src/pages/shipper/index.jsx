9f1695b57b5503693748a93da5040ac47afeeead
Hana Nabilah Fatinah <56430915+raihanahnabilah@users.noreply.github.com>
2022-08-13 12:00:10 +0700

    feat(layout): Create shipper layout

---
+++ src/pages/shipper/index.jsx
@@ -0,0 +1,12 @@
+import {Box, Button, Link, Text} from '@chakra-ui/react';
+import React from 'react';
+
+function Shipper() {
+    return (
+        <div>
+            Testing
+        </div>
+    );
+}
+
+export default Shipper;
--- src/route/index.js
+++ src/route/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 
 const Transporters = React.lazy(() => import('../pages/Transporters'));
 const Login = React.lazy(() => import('../pages/login/'));
+const Shipper = React.lazy(() => import('../pages/shipper'));
 
 export const routes = [
   {
     path: '/',
-    element: <Login />
+    element: <Shipper />
   }
 ];
 
 export const transporters=[
   {
-    path:'/',
+    path:'/transporter',
     element : <Transporters />
+  }
+]
+
+export const shipper=[
+  {
+    path:'/shipper',
+    element : <Shipper />
   }
 ]
